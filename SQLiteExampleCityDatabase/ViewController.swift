151f03bf85e4374eec96d3a3aa0fca08ce5b46b8
Bart van Kuik <bart@vankuik.nl>
2018-09-21 07:26:00 +0200

    Initial Commit

---
+++ SQLiteExampleCityDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SQLiteExampleCityDatabase
+//
+//  Created by Bart van Kuik on 21/09/2018.
+//  Copyright © 2018 DutchVirtual. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
